3d0903cf7be623cca6505bec884ec671b467303d
Jonathan Ma <jonathanjma03@gmail.com>
2024-02-02 17:53:01 -0500

    Store c strings in flash

--- src/MainControlLoop.cpp
+++ src/MainControlLoop.cpp
@@ -14,40 +14,40 @@ void MainControlLoop::execute()
 {
 
 #ifdef VERBOSE
-    Serial.println("-------------------- START LOOP --------------------");
+    Serial.println(F("-------------------- START LOOP --------------------"));
     if (sfr::imu::gyro_x->get_value(&val)) {
-        Serial.print("Gyro X: ");
+        Serial.print(F("Gyro X: "));
         Serial.println(val);
     }
     if (sfr::imu::gyro_y->get_value(&val)) {
-        Serial.print("Gyro Y: ");
+        Serial.print(F("Gyro Y: "));
         Serial.println(val);
     }
     if (sfr::imu::gyro_z->get_value(&val)) {
-        Serial.print("Gyro Z: ");
+        Serial.print(F("Gyro Z: "));
         Serial.println(val);
     }
     if (sfr::imu::acc_x->get_value(&val)) {
-        Serial.print("Accel X: ");
+        Serial.print(F("Accel X: "));
         Serial.println(val);
     }
     if (sfr::imu::acc_y->get_value(&val)) {
-        Serial.print("Accel Y: ");
+        Serial.print(F("Accel Y: "));
         Serial.println(val);
     }
     if (sfr::imu::acc_z->get_value(&val)) {
-        Serial.print("Accel Z: ");
+        Serial.print(F("Accel Z: "));
         Serial.println(val);
     }
     if (sfr::temperature::temp_c->get_value(&val)) {
-        Serial.print("Temperature (C): ");
+        Serial.print(F("Temperature (C): "));
         Serial.print(val);
-        Serial.println(" C");
+        Serial.println(F(" C"));
     }
     if (sfr::temperature::temp_f->get_value(&val)) {
-        Serial.print("Temperature (F): ");
+        Serial.print(F("Temperature (F): "));
         Serial.print(val);
-        Serial.println(" F");
+        Serial.println(F(" F"));
     }
     // if (sfr::gps::latitude->get_value(&val)) {
     //     Serial.print("GPS Latitude (degrees): ");
@@ -58,7 +58,7 @@ void MainControlLoop::execute()
     //     Serial.println(val);
     // }
     if (sfr::gps::altitude->get_value(&val)) {
-        Serial.print("GPS Alitude (m): ");
+        Serial.print(F("GPS Alitude (m): "));
         Serial.println(val);
     }
     // if (sfr::gps::utc_h->get_value(&val)) {
@@ -82,6 +82,6 @@ void MainControlLoop::execute()
     radio_control_task.execute();
 
 #ifdef VERBOSE
-    Serial.println("-------------------- END LOOP --------------------");
+    Serial.println(F("-------------------- END LOOP --------------------"));
 #endif
 }
